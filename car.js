a304c56a91d28e297e7080a3c7dc957d53c65b42
Moncayo1329 <86531347+Moncayo1329@users.noreply.github.com>
2023-05-27 03:31:38 +0100

    Add controls and Car.js

--- car.js
+++ car.js
@@ -1,2 +1,19 @@
+class Car {
+    constructor(x, y, width, height) {
+      this.x = x;
+      this.y = y;
+      this.width = width;
+      this.height = height;
 
+      this.control=new Controls();
+
+    }
   
+    draw(ctx) {
+      ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+}
+
+const car = new Car(100, 100, 30, 50);
+car.draw(ctx);
+
