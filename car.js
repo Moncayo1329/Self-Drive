8f96a41aae9b6b38359162c7aba8f1397954ea09
Moncayo1329 <86531347+Moncayo1329@users.noreply.github.com>
2023-05-27 19:15:49 +0100

    car.js no funciono

--- car.js
+++ car.js
@@ -1,23 +1,2 @@
-class Car {
-    constructor(x, y, width, height) {
-      this.x = x;
-      this.y = y;
-      this.width = width;
-      this.height = height;
 
-  this.control=new Controls();
-    }
-
-
-
-
-
-    
-    draw(ctx) {
-      ctx.fillRect(this.x, this.y, this.width, this.height);
-    }
-}
-
-const car = new Car(100, 100, 30, 50);
-car.draw(ctx);
 
--- main.js
+++ main.js
@@ -4,20 +4,39 @@ canvas.width = 200;
 
 const ctx = canvas.getContext("2d");
 
+class Car {
+  constructor(x, y, width, height) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
 
+    this.control = new Controls();
+  }
 
+  update(ctx) {
+    if (this.control.forward)
+      this.y -= 2;
 
-    
+    if (this.control.reverse)
+      this.y += 2;
 
+    this.draw(ctx);
+  }
 
+  draw(ctx) {
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+  }
+}
+
+const car = new Car(100, 100, 30, 50);
 
 function animate() {
-  car.update();
-  car.draw();
+  car.update(ctx);
   requestAnimationFrame(animate);
 }
 
-animate()
+animate();
 
 
 
